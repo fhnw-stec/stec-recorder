589527b1bb58aebf5d16bbbcac8c5ced4dd6c3e1
Rahel Lüthy <rahel.luethy@gmail.com>
2017-07-21 17:05:01 +0200

    #1: Project setup

---
+++ src/main/java/ch/fhnw/edu/stec/StecApp.java
@@ -0,0 +1,50 @@
+package ch.fhnw.edu.stec;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class StecApp extends Application {
+
+    private static final double WIDTH = 800;
+    private static final double HEIGHT = 600;
+    private static final String TITLE = "STEC";
+
+    private static final Logger LOG = LoggerFactory.getLogger(StecApp.class);
+
+    @Override
+    public void start(Stage stage) {
+
+        Scene scene = new Scene(new StackPane(new Label("Hey there :-)")));
+
+        stage.setTitle(TITLE);
+        stage.setScene(scene);
+        stage.setWidth(WIDTH);
+        stage.setHeight(HEIGHT);
+
+        stage.setOnCloseRequest(e -> {
+            LOG.trace("Main window closed, exiting application");
+            Platform.exit();
+            System.exit(0);
+        });
+
+        stage.show();
+    }
+
+    public static void main(String... args) {
+        Platform.setImplicitExit(true); // Exit VM if main frame is closed
+        try {
+            LOG.trace("Launching application");
+            launch(args);
+        } catch (Throwable t) {
+            LOG.error(t.getMessage(), t);
+            System.exit(-1);
+        }
+    }
+
+}
