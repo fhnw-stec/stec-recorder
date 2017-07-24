8819456b8ca70807ec5c9a9ca40534f47800395a
Rahel Lüthy <rahel.luethy@gmail.com>
2017-07-24 15:19:03 +0200

    #4: Init repo, committing .gitignore from template

--- src/main/java/ch/fhnw/edu/stec/StecController.java
+++ src/main/java/ch/fhnw/edu/stec/StecController.java
@@ -2,16 +2,29 @@ package ch.fhnw.edu.stec;
 
 import ch.fhnw.edu.stec.chooser.GigChooserController;
 import ch.fhnw.edu.stec.status.GigStatusController;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.RepositoryCache;
 import org.eclipse.jgit.util.FS;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Optional;
 
 final class StecController implements GigChooserController, GigStatusController {
 
     private static final Logger LOG = LoggerFactory.getLogger(StecController.class);
+
+    private static final String GIT_REPO = ".git";
+
+    private static final String GIT_IGNORE_TEMPLATE_FILE_NAME = "/gitignore-template.txt";
+    private static final String GIT_IGNORE_FILE_NAME = ".gitignore";
+    private static final String GIT_IGNORE_COMMIT_MSG = "Add " + GIT_IGNORE_FILE_NAME;
 
     private final StecModel model;
 
@@ -19,7 +32,7 @@ final class StecController implements GigChooserController, GigStatusController 
         this.model = model;
 
         this.model.gigDirectoryProperty().addListener((observable, oldRootDir, newRootDir) -> {
-            File gitRepo = new File(newRootDir, ".git");
+            File gitRepo = new File(newRootDir, GIT_REPO);
             boolean isInitialized = RepositoryCache.FileKey.isGitRepository(gitRepo, FS.detect());
             this.model.gigReady().setValue(isInitialized);
         });
@@ -40,7 +53,37 @@ final class StecController implements GigChooserController, GigStatusController 
 
     @Override
     public void initGig() {
-        LOG.debug("TODO: initGig");
+        if (!model.gigReady().get()) {
+            Optional<Git> optionalGit = initGitRepo(model.gigDirectoryProperty().get());
+            optionalGit.ifPresent(git -> {
+                model.gigReady().setValue(true);
+                commitGitIgnore(git);
+            });
+        }
+    }
+
+    private static Optional<Git> initGitRepo(File dir) {
+        try {
+            return Optional.ofNullable(Git.init().setDirectory(dir).call());
+        } catch (GitAPIException e) {
+            LOG.error("Git init failed.", e);
+        }
+        return Optional.empty();
+    }
+
+    private static void commitGitIgnore(Git git) {
+        try (InputStream gitIgnoreSource = StecController.class.getResourceAsStream(GIT_IGNORE_TEMPLATE_FILE_NAME)) {
+            File workTree = git.getRepository().getWorkTree();
+
+            File gitIgnoreTarget = new File(workTree, GIT_IGNORE_FILE_NAME);
+            Files.copy(gitIgnoreSource, gitIgnoreTarget.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            git.add().addFilepattern(GIT_IGNORE_FILE_NAME).call();
+
+            git.commit().setMessage(GIT_IGNORE_COMMIT_MSG).call();
+
+        } catch (IOException | GitAPIException e) {
+            LOG.error("Git commit .gitignore failed.", e);
+        }
     }
 
 }
