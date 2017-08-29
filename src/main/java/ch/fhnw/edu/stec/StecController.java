5dc5389ba8b48c62f1e4b589675c4b94f99f6d18
Mark Zeman <mark.zeman@students.fhnw.ch>
2017-08-29 11:43:58 +0200

    Added list of steps for #7, descriptions still missing

--- src/main/java/ch/fhnw/edu/stec/StecController.java
+++ src/main/java/ch/fhnw/edu/stec/StecController.java
@@ -3,8 +3,13 @@ package ch.fhnw.edu.stec;
 import ch.fhnw.edu.stec.chooser.GigChooserController;
 import ch.fhnw.edu.stec.status.GigStatusController;
 import io.vavr.control.Try;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.eclipse.jgit.api.AddCommand;
+import org.eclipse.jgit.api.CommitCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.RepositoryCache;
 import org.eclipse.jgit.util.FS;
 import org.slf4j.Logger;
@@ -15,6 +20,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 final class StecController implements GigChooserController, GigStatusController {
 
@@ -33,6 +41,13 @@ final class StecController implements GigChooserController, GigStatusController 
             File gitRepo = new File(newRootDir, GIT_REPO);
             boolean isInitialized = RepositoryCache.FileKey.isGitRepository(gitRepo, FS.detect());
             this.model.gigReady().setValue(isInitialized);
+            if (isInitialized){
+                Map snapshots = getSteps();
+                model.snapshots().setValue(snapshots);
+
+                ObservableList<String> tag_names = FXCollections.observableArrayList(new ArrayList(snapshots.keySet()));
+                model.snapshots_names().setValue(tag_names);
+            }
         });
 
         initModel();
@@ -81,6 +96,21 @@ final class StecController implements GigChooserController, GigStatusController 
 
         } catch (IOException | GitAPIException e) {
             LOG.error("Git commit .gitignore failed.", e);
+        }
+    }
+
+    private Map getSteps() {
+        try {
+            Git git = Git.open(model.gigDirectoryProperty().get());
+            Map<String, Ref> tags = git.getRepository().getTags();
+            LOG.debug("Got all tags");
+            for (String key : tags.keySet()) {
+                LOG.debug(key);
+            }
+            return tags;
+        } catch (Exception e){
+            LOG.error("Fetching all tags failed.", e);
+            return new HashMap<String, Ref>();
         }
     }
 
--- src/main/java/ch/fhnw/edu/stec/StecView.java
+++ src/main/java/ch/fhnw/edu/stec/StecView.java
@@ -4,6 +4,7 @@ import ch.fhnw.edu.stec.chooser.GigChooserView;
 import ch.fhnw.edu.stec.status.GigStatusView;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Window;
@@ -27,6 +28,11 @@ final class StecView extends VBox {
         TitledPane snapshotPane = new TitledPane(SNAPSHOT_SECTION_TITLE, new Group());
         snapshotPane.setCollapsible(false);
         snapshotPane.disableProperty().bind(model.gigReady().not());
+
+        ListView<String> list = new ListView<>();
+        model.snapshots_names().addListener((observable, oldValue, newValue) -> list.setItems(newValue));
+        snapshotPane.setContent(list);
+
 
         getChildren().addAll(gigPane, snapshotPane);
 
