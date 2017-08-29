d07f277a7f61191ce7dbaa051ad05fd77ad0f115
Mark Zeman <mark.zeman@students.fhnw.ch>
2017-08-29 15:55:59 +0200

    Made directory chooser editable with feedback to the user, working on #2

--- src/main/java/ch/fhnw/edu/stec/chooser/GigChooserView.java
+++ src/main/java/ch/fhnw/edu/stec/chooser/GigChooserView.java
@@ -6,6 +6,7 @@ import javafx.beans.property.ObjectProperty;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.stage.DirectoryChooser;
@@ -21,8 +22,18 @@ public final class GigChooserView extends HBox {
         setPadding(new Insets(5));
 
         TextField rootDirectoryValueField = new TextField();
-        rootDirectoryValueField.setEditable(false);
-        rootDirectoryValueField.textProperty().bind(StringBinding.stringExpression(gigDirectoryProperty));
+        rootDirectoryValueField.setEditable(true);
+        rootDirectoryValueField.setText(gigDirectoryProperty.get().getAbsolutePath());
+        rootDirectoryValueField.focusedProperty().addListener((arg0, oldVal, newValue) -> {
+            File file = new File(rootDirectoryValueField.getText());
+            if (!file.exists() || !file.isDirectory()){
+                rootDirectoryValueField.setStyle("-fx-background-color: rgba(255, 0, 0, 0.3);");
+            }
+            else if (file.exists() && file.isDirectory()){
+                rootDirectoryValueField.setStyle("");
+                gigDirectoryProperty.setValue(file);
+            }
+        });
 
         DirectoryChooser directoryChooser = new DirectoryChooser();
         directoryChooser.initialDirectoryProperty().bind(gigDirectoryProperty);
