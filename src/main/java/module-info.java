920b38a022c4000ebcb5edbab695fe2d1f975f84
Rahel Lüthy <rahel.luethy@gmail.com>
2017-10-25 14:08:56 +0200

    #13: Towards using jlink for packaging (wip)

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module ch.fhnw.edu.stec {
+
+    exports ch.fhnw.edu.stec;
+
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.web;
+
+    requires vavr;
+    requires asciidoctorj;
+    requires org.slf4j;
+    requires controlsfx;
+
+}
